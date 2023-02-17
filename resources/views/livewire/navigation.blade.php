90882322a35b7eb89e516d7b5bfa4debc5d6fae2
axel paz <axeldavidpaz@gmail.com>
2023-02-17 02:21:51 -0300

    test deploy in cloudway

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -92,7 +92,7 @@
 
           @else 
           <div>
-            <a href="{{route("login")}}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
+            <a href="{{route("login")}}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Loginn</a>
             <a href="{{route("register")}}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
           </div>
           @endauth
