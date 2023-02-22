2010d540f85178f098db2eb195e8c16469cf7dce
axel paz <axeldavidpaz@gmail.com>
2023-02-22 19:54:26 -0300

    index-bl-

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-
-<div class="mx-auto  w-full px-8 sm:px-6  bg-red-500 py-8">
+<h1>index blade php naaa</h1>
+{{-- <div class="mx-auto  w-full px-8 sm:px-6  bg-red-500 py-8">
 
 
   <div class="w-4/5  grid  grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
     @foreach ($posts as $post)
 
-     <article class="w-full h-80 bg-cover bg-center rounded-md shadow-ls" style="background-image: url(@if($post->image) {{Storage::url($post->image->url)}} @else https://cdn.pixabay.com/photo/2016/07/22/14/12/bike-1534902_640.jpg @endif)">
-     {{-- <article class="w-full h-80 bg-cover bg-center rounded-md shadow-ls @if($loop->first)md:col-span-2  @endif" style="background-image: url(@if($post->image) {{Storage::url($post->image->url)}} @else https://cdn.pixabay.com/photo/2016/07/22/14/12/bike-1534902_640.jpg @endif)"> --}}
+     
+     <article class="w-full h-80 bg-cover bg-center rounded-md shadow-ls @if($loop->first)md:col-span-2  @endif" style="background-image: url(@if($post->image) {{Storage::url($post->image->url)}} @else https://cdn.pixabay.com/photo/2016/07/22/14/12/bike-1534902_640.jpg @endif)">
       
-      {{-- <h2 class="bg-red-100">{{$post->image->url}} :::::: {{Storage::url($post->image->url)}}</h2> --}}
+      
       <div class="w-full h-full px-8 flex flex-col justify-center">
           {{-- tagsss --}}
       <div>
@@ -30,5 +30,5 @@
 
  <div class="mt-4">{{$posts->links( )}}</div>
 
-</div> 
+</div>  --}}
 </x-app-layout>
