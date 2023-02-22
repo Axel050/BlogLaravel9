8746ac5c5e87c8924c601ca3f6eacb4fafda5e78
axel paz <axeldavidpaz@gmail.com>
2023-02-22 20:01:51 -0300

    index -blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
-<h1>index blade php naaa</h1>
-{{-- <div class="mx-auto  w-full px-8 sm:px-6  bg-red-500 py-8">
+
+  <h1>inde clean </h1>
+{{-- 
+<div class="mx-auto  w-full px-8 sm:px-6  bg-red-500 py-8">
 
 
   <div class="w-4/5  grid  grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -8,17 +10,19 @@
 
      
      <article class="w-full h-80 bg-cover bg-center rounded-md shadow-ls @if($loop->first)md:col-span-2  @endif" style="background-image: url(@if($post->image) {{Storage::url($post->image->url)}} @else https://cdn.pixabay.com/photo/2016/07/22/14/12/bike-1534902_640.jpg @endif)">
+       --}}
+
+      {{-- <h2 class="bg-red-100">{{$post->image->url}} :::::: {{Storage::url($post->image->url)}}</h2> --}}
+
+      {{-- <div class="w-full h-full px-8 flex flex-col justify-center">
       
-      
-      <div class="w-full h-full px-8 flex flex-col justify-center">
-          {{-- tagsss --}}
       <div>
         @foreach ($post->tags as $tag)
-        {{-- <a href="" class="inline-block px-3 h-6   bg-purple-600 text-white rounded-full m-1">{{$tag->name}}</a> --}}
+        
         <a href="{{route("posts.tag", $tag)}}" class="inline-block px-3 h-6   bg-{{$tag->color}}-600 text-white rounded-full m-1">{{$tag->name}}</a>
         @endforeach
       </div>
-      {{-- Title --}}
+      
       <h1 class= "text-4xl text-white leading-9 font-bold ">
         <a href="{{route('posts.show', $post)}}">{{$post->name}}</a>
         </h1>
