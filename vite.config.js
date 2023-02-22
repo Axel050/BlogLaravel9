514c2ca2d72400bbb2c4dfa76b11e76001d7d587
axel paz <axeldavidpaz@gmail.com>
2023-02-22 17:04:41 -0300

    viteconf refu

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
-            refresh: true
+            hotFile: 'storage/vite.hot', // Customize the "hot" file...
+            buildDirectory: 'bundle', // Customize the build directory...
+            input: ['resources/js/app.js'] // Specify the entry points...
         })
-    ]
+    ],
+    build: {
+        manifest: 'assets.json' // Customize the manifest filename...
+    }
 })
